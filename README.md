2758a211bedc006ecc10e9ddfac0c5f7c2b370a3
Leonardo Lima <leonardo403@gmail.com>
2016-10-19 20:24:05 -0200

    change important

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# checkout-form
+# Checkout-form
+
+#NPM Install
+
+```
+ npm install
+```
+
+#Bower install
+
+```
+bower install
+```
+
+#Run localhost
+
+```
+Finally, run npm start to run Gulp
+```
---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).foundation();
+
+/* Card.js plugin by Jesse Pollak. https://github.com/jessepollak/card */
+$('form').card({
+    container: '.card-wrapper',
+    width: 280,
+
+    formSelectors: {
+        nameInput: 'input[name="first-name"], input[name="last-name"]'
+    }
+});
